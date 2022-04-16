1c2506db923fa45d4295bdfbcafa434f497769d7
amiribnjaman <amiribnjaman@gmail.com>
2022-04-17 01:37:12 +0600

    Nav bar designed implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import NavBar from './Pages/Shared/NavBar/NavBar';
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home/Home'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
+import Logo from '../../../Assets/img/logo.png'
+import './NavBar.css'
+import { NavLink } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const NavBar = () => {
     const [showMenu, setShowMenu] = useState(false)
+
     return (
         <div>
-
-
             <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-                <div className="container flex flex-wrap justify-between items-center mx-auto">
-                    <a href="https://flowbite.com" className="flex items-center">
-                        <img src="/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                    </a>
-                    <div className="flex md:order-2">
-                        <div className="hidden relative mr-3 md:mr-0 md:block">
-                            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-                            </div>
-                            <input type="text" id="email-adress-icon" className="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                <div className="containe pt-1 w-11/12 flex flex-wrap justify-between items-center mx-auto">
+                    <NavLink to='/' className="flex items-center">
+                        <div className='w-1/3'>
+                            <img className='w-11/12' src={Logo} alt="" />
                         </div>
+                        <div className='logo-text text-left'>
+                            <h4>SMART <span className='text-red-600 font-bold'>GYM</span> TRAINER</h4>
+                        </div>
+                    </NavLink>
+                    <div className="md:hidden flex md:order-2">
                         <button onClick={() => setShowMenu(!showMenu)} data-collapse-toggle="mobile-menu-3" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-3" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
@@ -26,15 +27,18 @@ const NavBar = () => {
                         </button>
                     </div>
                     <div className={`${showMenu ? 'block' : 'hidden'} justify-between items-center w-full md:flex md:w-auto md:order-1`} id="mobile-menu-3">
-                        <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <ul className="flex items-center flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                                <CustomLink to='/'>Home</CustomLink>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                                <CustomLink to='/about'>About Me</CustomLink>
                             </li>
                             <li>
-                                <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                                <CustomLink to='/blog'>Blog</CustomLink>
+                            </li>
+                            <li>
+                                <NavLink to="#" type='button' className="block bg-red-600 text-white rounded-full px-10 py-2.5 hover:bg-red-700" aria-current="page">Login</NavLink>
                             </li>
                         </ul>
                     </div>
