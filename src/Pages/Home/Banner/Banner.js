3e77b497ac116f85beb228e926baf7fa458f7ff2
amiribnjaman <amiribnjaman@gmail.com>
2022-04-17 12:18:22 +0600

    Banner component added and responsive checked

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import BannerImg from '../../../Assets/img/banner.png';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner-section'>
+            <img src={BannerImg} alt="" />
+            <div className='banner-text md:top-1/2 lg:top-1/3 top-1/2 md:left-1/2 left-3.5 sm:left-36 md:w-2/6 '>
+                <h2 className='text-white text-2xl md:text-5xl font-bold font-sans'>KEEP YOUR BODY FIT & <span className='text-red-600'>STRONG</span></h2>
+                <p className='text-slate-200 mt-2 text-sm'>with SMART <span className='text-red-500'>GYM</span> TRAINER</p>
+                <div className='md:mt-5 mt-1'>
+                    <button className="block mx-auto bg-red-500 text-white font-semibold rounded-lg px-14 py-2.5 hover:bg-red-600">Explore More</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,40 +1,40 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import Logo from '../../../Assets/img/logo.png';
+import { Link, NavLink } from 'react-router-dom';
+// import Logo from '../../../Linkssets/img/logo.png';
 
 const Footer = () => {
     const date = new Date()
     const currentYear = date.getFullYear()
     return (
         <div>
-            <footer className="p-4 bg-gray-200 rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+            <footer className="p-4 bg-gray-200 rounded-lg shadow md:px-6 py-8 dark:bg-gray-800">
                 <div className=' w-11/12 mx-auto'>
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <NavLink to='/' className="md:flex block text-center items-center">
                             {/* <div className='w-1/2'>
                                 <img className='w-full' src={Logo} alt="" />
                             </div> */}
-                            <div className='font-semibold'>
+                            <div className='font-semibold mb-1 md:mb-0'>
                                 <h4>SMART <span className='text-red-600 font-bold'>GYM</span> TRAINER</h4>
                             </div>
                         </NavLink>
                         <ul className="flex justify-center flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                             <li>
-                                <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                                <Link to="#" className="mr-4 hover:underline md:mr-6 ">About</Link>
                             </li>
                             <li>
-                                <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                                <Link to="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
                             </li>
                             <li>
-                                <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                                <Link to="#" className="mr-4 hover:underline md:mr-6 ">Licensing</Link>
                             </li>
                             <li>
-                                <a href="#" className="hover:underline">Contact</a>
+                                <Link to="#" className="hover:underline">Contact</Link>
                             </li>
                         </ul>
                     </div>
-                    <hr className="my-6 border-gray-400 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                    <span className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; {currentYear} <a href="https://flowbite.com" className="hover:underline"><h4 className='ml-1'> SMART <span className='text-red-600 font-bold'>GYM</span> TRAINER</h4></a>. All Rights Reserved.
+                    <hr className="my-6 border-gray-400 sm:mx-auto lg:my-8" />
+                    <span className="flex justify-center text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; {currentYear} <Link to="/" className="hover:underline"><h4 className='ml-1'> SMART <span className='text-red-600 font-bold'>GYM</span> TRAINER</h4></Link>. All Rights Reserved.
                     </span>
                 </div>
             </footer>
